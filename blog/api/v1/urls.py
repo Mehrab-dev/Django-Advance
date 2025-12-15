0437a370971b544d1c5248def062a39a1db669fe
Mehrab <programmer.py.mail@gmail.com>
2025-12-16 02:01:51 +0330

    creating serializer.py file and creating api_postdetail in blog.api.v1.views

--- blog/api/v1/urls.py
+++ blog/api/v1/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'api_v1'
 
 urlpatterns = [
-    path('post/',views.api_post_list,name='post_api')
+    path('post/',views.api_post_list,name='post_api'),
+    path('post/<int:pk>/',views.api_postdetail,name='api_post_detail')
 
 ]
--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -1,6 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from blog.models import Post
+from .serializers import PostSerializers
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 @api_view()
 def api_post_list(request) :
-    return Response("Hello")
+    posts = Post.objects.all()
+    serializer = PostSerializers(posts,many=True)
+    return Response(serializer.data)
+
+@api_view()
+def api_postdetail(request,pk) :
+    # post = get_object_or_404(Post,pk=pk)
+    # serializer = PostSerializers(post)
+    # return Response(serializer.data)
+
+    try :
+        post = Post.objects.get(pk=pk)
+        serializer = PostSerializers(post)
+        return Response(serializer.data)
+    except Post.DoesNotExist :
+        return Response({"detail":"post does not exists"},status=404)
