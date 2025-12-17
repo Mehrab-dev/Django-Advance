3e8a598d41ed24df415e2135455a46a24dc0a791
Mehrab <programmer.py.mail@gmail.com>
2025-12-17 13:33:56 +0330

    adding different methods to functions in views

--- blog/api/v1/serializers.py
+++ blog/api/v1/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
+from blog.models import Post
 
 
-class PostSerializers(serializers.Serializer) :
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+# class PostSerializers(serializers.Serializer) :
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+
+
+class PostSerializers(serializers.ModelSerializer) :
+
+    class Meta :
+        model = Post
+        fields = ['id','title','status','category','author','created_date','published_date']
--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -5,21 +5,45 @@ from .serializers import PostSerializers
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 
-@api_view()
+@api_view(["GET","POST"])
 def api_post_list(request) :
-    posts = Post.objects.all()
-    serializer = PostSerializers(posts,many=True)
-    return Response(serializer.data)
+    if request.method == 'GET' :
+        posts = Post.objects.all()
+        serializer = PostSerializers(posts,many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST' :
+        serializer = PostSerializers(data=request.data)
+        if serializer.is_valid() :
+            serializer.save()
+            return Response(request.data)
+        else :
+            return Response(serializer.errors)
 
-@api_view()
+
+
+@api_view(["GET","PUT","DELETE"])
 def api_postdetail(request,pk) :
     # post = get_object_or_404(Post,pk=pk)
     # serializer = PostSerializers(post)
     # return Response(serializer.data)
 
+
     try :
-        post = Post.objects.get(pk=pk)
-        serializer = PostSerializers(post)
-        return Response(serializer.data)
+        if request.method == 'GET' :
+            post = Post.objects.get(pk=pk)
+            serializer = PostSerializers(post)
+            return Response(serializer.data)
+        elif request.method == 'PUT' :
+            post = Post.objects.get(pk=pk)
+            serializer = PostSerializers(post,data=request.data)
+            if serializer.is_valid() :
+                serializer.save()
+                return Response(serializer.data)
+            else :
+                return Response(serializer.errors)
+        elif request.method == "DELETE" :
+            post = Post.objects.get(pk=pk)
+            post.delete()
+            return Response({"detail":"item removed successfully"},status=status.HTTP_204_NO_CONTENT)
     except Post.DoesNotExist :
         return Response({"detail":"post does not exists"},status=404)
